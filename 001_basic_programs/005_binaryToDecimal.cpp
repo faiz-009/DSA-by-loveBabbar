939fc27114b1b638bc9bf9d50738479e918ad0a6
MOHD. FAIZ QURESHI <mdfaizqureshi09@gmail.com>
2022-09-02 00:17:08 +0530

    c

--- 001_basic_programs/005_binaryToDecimal.cpp
+++ 001_basic_programs/005_binaryToDecimal.cpp
@@ -1,4 +1,48 @@
 /*
-    
+    i/p : 7
+    o/p : 111
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int n){
+
+    int rev = 0;
+    while(n){
+
+        rev = rev*10 + n%10;
+        n = n/10;
+    }
+    return rev;
+}
+
+void decimalToBinary(int n){
+
+    int arr[10] ={0};
+    int i=0;
+    while(n){
+        arr[i] = n%2;
+        n = n/2;
+        i++;
+    }
+    
+    for(int j=i;j>=0;j--){
+        cout<<arr[j];
+    }
+    cout<<endl;
+    
+}
+
+int main(){
+    
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    //cout<<"Binary of "<<n<<" is: "<<decimalToBinary(n)<<endl;
+    decimalToBinary(n);
+
+return 0;
+}
