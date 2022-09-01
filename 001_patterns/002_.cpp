1739f43ae0ce153cb99fc3dfdf3f7f136c258eb8
MOHD. FAIZ QURESHI <mdfaizqureshi09@gmail.com>
2022-09-01 10:03:48 +0530

    c

---
+++ 001_patterns/002_.cpp
@@ -0,0 +1,26 @@
+/*
+    1 1 1
+    2 2 2 
+    3 3 3
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+
+        for(int j=0;j<n;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+
+return 0;
+}
